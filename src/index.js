7401088026086457af0dc29b7fc2248d0f93295c
Alice <doritosbae@mail.ru>
2022-12-13 20:16:23 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.html";
-import "./style.css";
-import { calculator } from "./modules/operations";
-import { themeSwitcher } from "./modules/themeChange";
-
-window.onload = () => {
-  themeSwitcher();
-  calculator();
-};
